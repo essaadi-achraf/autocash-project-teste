466f3ae95e70147ed5677ea5226ced3087965ac3
Achraf <achraf.essaadi00@email.com>
2026-01-02 12:42:13 +0100

    Change hello message to test CD

--- src/main/java/com/autocash/controller/HelloController.java
+++ src/main/java/com/autocash/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return  "return from achraf ";
+        return  "hello from autocash(ci\cd) ";
     }
 }
